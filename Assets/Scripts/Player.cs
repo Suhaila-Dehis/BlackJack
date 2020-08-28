a4e7f7df803f0e4706e8a4c679ac3fe2fda1f6f0
Suhaila Dehis <suhaila.dehis@yahoo.com>
2020-08-28 16:03:57 -0300

    init commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public string playerName;
+    public int playerCash;
+    public int playerBet;
+    public int playerHand;
+    public List<PlayingCard> Playercards;
+
+    public int CalculateHandValue()
+    {
+        playerHand = 0;
+        foreach (var card in Playercards)
+        {
+            playerHand += card.value;
+        }
+        return playerHand;
+    }
+
+}
